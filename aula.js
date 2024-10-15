33f665410e7d9a6d744e52f075bb88563e74e00d
matheus-freire <159149078+matheus-freire@users.noreply.github.com>
2024-10-15 12:05:17 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,41 @@
+function calcularSaldo(vitorias, derrotas) {
+    
+    const saldoVitorias = vitorias - derrotas;
+
+  
+    let nivel = "";
+
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias >= 101) {
+        nivel = "Imortal";
+    }
+
+
+    return { saldoVitorias, nivel };
+}
+
+
+function exibirResultado(vitorias, derrotas) {
+    
+    const resultado = calcularSaldo(vitorias, derrotas);
+
+ 
+    console.log(`O Herói tem um saldo de ${resultado.saldoVitorias} e está no nível ${resultado.nivel}`);
+}
+
+
+const vitorias = 55;  
+const derrotas = 20;  
+exibirResultado(vitorias, derrotas);
